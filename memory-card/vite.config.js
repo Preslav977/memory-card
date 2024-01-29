b8a636e8a09d69c46996ae8c5b4c90791f8b3014
Preslav977 <p.cvetanow@gmail.com>
2024-01-29 12:40:30 +0200

    Added some config lines in vite config to see if it would fix an error

--- memory-card/vite.config.js
+++ memory-card/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2022",
+  },
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
   plugins: [react()],
-})
+});
