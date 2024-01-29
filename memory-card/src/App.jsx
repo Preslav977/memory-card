e5f5cdbd8fe3bc3fe342c38cb751582ff5aa828a
Preslav977 <p.cvetanow@gmail.com>
2024-01-29 10:06:02 +0200

    The two components are imported and the array is rendering the pokemons

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -4,15 +4,31 @@ import { pokemonImages } from "./api/FetchPokemons";
 
 import { HeaderComponent } from "./components/HeaderComponent";
 
+import { MainComponent } from "./components/MainComponent";
+
 import "./App.css";
 
 function App() {
   return (
-    <HeaderComponent
-      gameName="Pokemon Memory Game"
-      gameInstructions="Get points by clicking on the pokemon card, 
+    <>
+      <HeaderComponent
+        gameName="Pokemon Memory Game"
+        gameInstructions="Get points by clicking on the pokemon card, 
       but don't click it more than once"
-    ></HeaderComponent>
+      ></HeaderComponent>
+      <MainComponent>
+        <div className="cardContainer">
+          {pokemonImages.map((pokemon) => (
+            <div className="card" key={pokemon.id}>
+              <div className="imgContainer">
+                <img src={pokemon.img} alt={pokemon.name} />
+              </div>
+              <p className="pokemonName">{pokemon.name}</p>
+            </div>
+          ))}
+        </div>
+      </MainComponent>
+    </>
   );
 }
 
